7a23672e3eeaafcc1c58d155ba1e4ad22499a420
dev_uoon <104570636+devuoon@users.noreply.github.com>
2024-05-18 18:59:57 +0900

    [#54]Feat: 팀페이지 생성
    모달창 링크 연결
    파일 생성 및 구조 변경
    recruit_list.html 레이아웃 변경
    
    * 칸반보드 수정 중 푸쉬

--- common/header.js
+++ common/header.js
@@ -9,6 +9,7 @@ const renderHeader = () => {
       <ul class="menu">
         <li><a href="/recruit_list.html">멤버 모집</a></li>
         <li><a href="/show_list.html">프로젝트 자랑</a></li>
+        <li><a href="/team_note_list.html">팀 노트</a></li>
       </ul>
     </nav>
     <ul class="member">
